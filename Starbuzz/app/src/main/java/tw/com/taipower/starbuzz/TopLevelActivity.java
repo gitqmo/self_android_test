816c746848417f389124449e1bd4d2f0060ad657
gitqmo <studentyien@gmail.com>
2016-03-17 11:32:11 +0800

    Starbuzz Example
    已做完Chapter6的隨堂測驗

--- Starbuzz/app/src/main/java/tw/com/taipower/starbuzz/TopLevelActivity.java
+++ Starbuzz/app/src/main/java/tw/com/taipower/starbuzz/TopLevelActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -18,8 +19,11 @@ public class TopLevelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_level);
 
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> listView, View view, int positon, long id){
-                if(positon == 0){
+            public void onItemClick(AdapterView<?> listView,
+                                    View view,      //被點擊的項目視圖元件(item view)
+                                    int position,   //在列表視圖裡的位置(position)
+                                    long id){       //底層資料的列ID(row id)
+                if(position == 0){
                     Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
                     startActivity(intent);
                 }
@@ -29,9 +33,18 @@ public class TopLevelActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
 
-
+        //隨堂測驗
+        //------------------------------------------------------------------------------------------
         String[] colors = {"Pink", "Red", "Orange", "Yellow", "Green", "Blue", "Purple"};
-        //Spinner spinner = findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                colors
+        );
+        spinner.setAdapter(adapter);
+        //------------------------------------------------------------------------------------------
+
 
     }
 }
