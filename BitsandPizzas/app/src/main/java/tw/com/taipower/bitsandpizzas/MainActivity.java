39795f904b018495bacf551bc00d516be8f16b33
gitqmo <studentyien@gmail.com>
2016-04-04 20:56:50 +0800

    Ch10 Bits and Pizzas Example
    新增Nagivation Drawer成功。

--- BitsandPizzas/app/src/main/java/tw/com/taipower/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/tw/com/taipower/bitsandpizzas/MainActivity.java
@@ -1,18 +1,36 @@
 package tw.com.taipower.bitsandpizzas;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
 public class MainActivity extends LogTraceActivity {
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+
+        this.titles = this.getResources().getStringArray(R.array.titles);
+        this.drawerList = (ListView) this.findViewById(R.id.drawer);
+        this.drawerList.setAdapter(
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        this.titles));
+        this.drawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     /**
@@ -65,5 +83,66 @@ public class MainActivity extends LogTraceActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /**
+     * 依據被點擊的選項呈現不同的功能選單。
+     *
+     * @param position
+     */
+    private void selectIem(int position) {
+        //切換所對應之功能選單
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+
+        //設定Action Bar的標題
+        this.setActionBarTitle(position);
+
+        //關閉Drawer
+        DrawerLayout drawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(this.drawerList);
+    }
+
+    /**
+     * 當Navigation Drawer被點擊時，呈現的所對應之功能選單。
+     */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            MainActivity.this.selectIem(position);
+        }
+    }
+
+    /**
+     * 設定Action Bar的標題
+     * @param position
+     */
+    private void setActionBarTitle(int position){
+        String title;
+        if(position == 0){
+            title = this.getResources().getString(R.string.app_name);
+
+        }else{
+            title = this.titles[position];
+        }
+        this.getActionBar().setTitle(title);
     }
 }
---
+++ BitsandPizzas/app/src/main/java/tw/com/taipower/bitsandpizzas/PastaFragment.java
@@ -0,0 +1,37 @@
+package tw.com.taipower.bitsandpizzas;
+
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PastaFragment extends ListFragment {
+
+
+    public PastaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                this.getResources().getStringArray(R.array.pasta));
+        this.setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
